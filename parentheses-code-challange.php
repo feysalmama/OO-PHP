ace56a99896c93f3f420487e838e2d7c2505b674
Feysal Mama <feysaloo0922@gmail.com>
2021-12-09 13:00:18 +0300

    parentheses balancing check source code done

---
+++ parentheses-code-challange.php
@@ -0,0 +1,68 @@
+<?php
+
+ class Solution{
+    public $charactorArray = ['(',')','{','}','[',']'];
+    public $stack = [];
+
+    public function isValidClose($open){
+      
+        if($open=="(" || $open=="{" ||$open=="["){
+            array_push($this->stack,$open);
+          
+        }else{
+            if(count($this->stack)==0){
+                return false;
+            }
+           
+            if($open==")"){
+                $removed = array_pop($this->stack);
+                if($removed== "{" || $removed == "["){
+                    return false;
+                }
+                
+            }
+            if($open=="]"){
+                $removed = array_pop($this->stack);
+                if($removed== "{" || $removed == "("){
+                    return false;
+                }
+                
+            }
+            if($open=="}"){
+                $removed = array_pop($this->stack);
+                if($removed== "(" || $removed == "["){
+                    return false;
+                }
+                
+            }
+        }
+
+        return true;
+        
+    }
+
+     public function isValid($s)
+     {
+         $string_array= str_split($s);
+        if(strlen($s) >=1 && strlen($s) <=104 && strlen($s)%2==0){  
+            for($i=0; $i < count($string_array);$i++){
+                if(!in_array($string_array[$i],$this->charactorArray)){
+                    return "wrong charctor, not valid";
+                }
+            }
+            for($i=0; $i < count($string_array);$i++){
+                if(!$this->isValidClose($string_array[$i])){
+                   return "wrong openning and closing";
+                }
+            }
+
+            return " is valid";
+         }else{
+            return " the string must be >=1 and <= 104, and also must be closed,not valid";
+        }   
+     }
+ }
+ $solution  = new Solution();
+ echo $solution->isValid("[(){({})}]{}")
+
+?>
